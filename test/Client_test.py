20e78fa4f3d0afb63feeb1f586b59167c32bb511
Diego Ramirez Caballero <diego.fer659@gmail.com>
2020-10-23 07:59:47 -0500

    Update Client_test.py

--- test/Client_test.py
+++ test/Client_test.py
@@ -13,6 +13,10 @@ class ClientTestCase(unittest.TestCase):
 
     def test_name_add_preexistence(self):
         self.assertEqual(len(self.test_client.get_all_preexistence()), 0)
+        self.test_client.add_preexistence('mock preexistence')
+        self.assertEqual(len(self.test_client.get_all_preexistence()), 1)
+        self.assertEqual(self.test_client.get_preexistence(0), 'mock preexistence')
+        self.assertEqual(self.test_client.get_preexistence(1), 'There is no such preexistence')
         
 
     def test_name_remove_preexistence(self):
@@ -23,9 +27,9 @@ class ClientTestCase(unittest.TestCase):
         self.test_client.add_preexistence('mock preexistence 3')
         self.assertEqual(len(self.test_client.get_all_preexistence()), 3)
         self.test_client.remove_preexistence(1)
-        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence')
-        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence 3')
-        self.assertEqual(self.test_client.get_preexistence(2), 'There is no such preexistence')
+        self.assertEqual(self.test_client.get_preexistence(2), 'mock preexistence')
+        self.assertEqual(self.test_client.get_preexistence(3), 'mock preexistence 3')
+        self.assertEqual(self.test_client.get_preexistence(1), 'There is no such preexistence')
 
 
 if __name__ == '__main__':
